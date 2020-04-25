29a1cb59e0367f2a70036837dc38a10f8b3f72ba
matbi86 <chacagea@naver.com>
2020-04-25 16:14:20 +0900

    added mem_gen proj

---
+++ zybo_z7_20/02_memory_gen/memory_gen.ip_user_files/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
@@ -0,0 +1,32 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Sat Apr 25 15:34:59 2020
+// Host        : joohankim86 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               d:/study/git_study/memory_gen/memory_gen.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
+// Design      : blk_mem_gen_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_4,Vivado 2019.2" *)
+module blk_mem_gen_0(clka, ena, wea, addra, dina, douta, clkb, enb, web, addrb, 
+  dinb, doutb)
+/* synthesis syn_black_box black_box_pad_pin="clka,ena,wea[0:0],addra[10:0],dina[23:0],douta[23:0],clkb,enb,web[0:0],addrb[10:0],dinb[23:0],doutb[23:0]" */;
+  input clka;
+  input ena;
+  input [0:0]wea;
+  input [10:0]addra;
+  input [23:0]dina;
+  output [23:0]douta;
+  input clkb;
+  input enb;
+  input [0:0]web;
+  input [10:0]addrb;
+  input [23:0]dinb;
+  output [23:0]doutb;
+endmodule
